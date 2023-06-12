b1fbc41491842a19040146498a3c628fe3afbcab
Abel Alejandro Nieva <abelnieva@cloud.upwork.com>
2023-06-12 20:01:01 +0200

    bump version

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 module "cluster_infra" {
   source                         = "app.terraform.io/security-framework/eks-security-clusters/aws"
-  version                        = "0.0.8"
+  version                        = "0.0.10"
   cluster_name                   = "test-cluster"
   vpc_cidr                       = "10.0.0.0/16"
   cluster_endpoint_public_access = false
