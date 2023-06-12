7eb143166fbc5eba318d4428213542aee30ee6ec
Abel Alejandro Nieva <abelnieva@cloud.upwork.com>
2023-06-12 20:21:01 +0200

    set tf cloud agent instance type

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ module "cluster_infra" {
   ssh_public_key                 = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrS3CYVw9tqM18WJIi4XVOgKbuh0BV/sJe1fRq8E1r+IorSRBwH6H5kAg0zEODdXnLDD3YnZOs4SG5WTEuNtO7AsGuaKRe1aIyxOsTBoyEY+JcNbCj76Z5ISven3LQj6AeUbGTDG6VA9gE6F3EwmD9Ip3vjRDzrlkG/XIs1URtMlFXfFrEemCeNuuw50MQA2VcY4HVpZYI3gNZY1iufwPMlSDDoqTShOhqJZDh5QxhdGvwKjnISOMgjbELnANeaaeP9DQzyzMSI9Y4u65anBII1afn6cnqpBgBM52A8iLi8SeCmyyYGoCv+H3/bQTv1z1KUiqIsc1mVy3eNYD6olPy9REslGpJwd4huEmkKou7BA7NWS5k/EBg78XC8KSL8vZSrYvWiiCjz7YG0pUP14O6/Gn5b1aYtsTTNvNsZo6R6IIzn8jA+RPYdKEU8dSKMp4+A9EY3KBUhfbtgc+bGwyQYEIJBhidmrZc9oip8/JngQYcogP7jEuG0DH3i69+OHs= abelnieva@abels-MBP.home"
   cluster_endpoint_public_access = true
   tfc_agent_token                = var.tfc_agent_token
+  tfc_agent_instance_type        = "t3.large"
   ecr_repos_list                 = ["testrepo"]
   dev_teams = {
     dev_1_team = {
