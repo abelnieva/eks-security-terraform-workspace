18d1ef719d9f8fee2960e4bd95a5b3cd286f1986
Abel Alejandro Nieva <abelnieva@cloud.upwork.com>
2023-06-19 18:06:26 +0200

    removing not needed files

--- scripts/setupterraformconfig.sh
+++
@@ -1,9 +0,0 @@
-source config.sh
-echo "remote state backup"
-cp remote_backend.tf remote_backend.tf.back
-sed "s/#{organization_name}/${organization_name}/"
-sed -i "s/\#{organization_name}/$organization_name/g" remote_backend.tf
-sed -i "s/\#{workspace_name}/$workspace_name/g" remote_backend.tf
-
-
-echo $config
